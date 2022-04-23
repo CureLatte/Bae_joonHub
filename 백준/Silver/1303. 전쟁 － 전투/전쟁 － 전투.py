e0da2b0524386fbb32458be5bd1ca4fcb232411a
zhqmfkv <83353283+CureLatte@users.noreply.github.com>
2022-04-23 15:35:14 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 824 ms, Memory: 31352 KB -BaekjoonHub

---
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -0,0 +1,85 @@
+input_data = input()
+
+col = int(input_data.split(' ')[0]) - 1
+row = int(input_data.split(' ')[1]) - 1
+graph = []
+
+for i in range(row + 1):
+    a = input()
+    graph.append(list(a))
+
+# W 병사
+
+# 첫번째 시작점 찾기
+
+all_W_position = []
+
+for x, row_ in enumerate(graph):
+    for y, target in enumerate(row_):
+        if target == 'W':
+            all_W_position.append((x, y))
+
+
+answer = 0
+for next_network in all_W_position:
+    if len(all_W_position) == 0:
+        break
+    if next_network not in all_W_position:
+        continue
+    stack = [next_network]
+    visited = []
+    while len(stack) != 0:
+        curr_x, curr_y = stack.pop()
+        if (curr_x, curr_y) in visited:
+            continue
+        visited.append((curr_x, curr_y))
+        if curr_x + 1 <= row and graph[curr_x + 1][curr_y] == "W" and (curr_x + 1, curr_y) not in visited:
+            stack.append((curr_x+1, curr_y))
+        if curr_x - 1 >= 0 and graph[curr_x - 1][curr_y] == "W" and (curr_x - 1, curr_y) not in visited:
+            stack.append((curr_x-1, curr_y))
+        if curr_y + 1 <= col and graph[curr_x][curr_y + 1] == 'W' and (curr_x, curr_y + 1) not in visited:
+            stack.append((curr_x, curr_y+1))
+        if curr_y - 1 >= 0 and graph[curr_x][curr_y -1] == "W" and (curr_x, curr_y - 1) not in visited:
+            stack.append((curr_x, curr_y-1))
+    answer += len(visited)**2
+
+    all_W_position = list(set(all_W_position) - set(visited))
+
+print(answer)
+
+# B 병사
+
+
+all_W_position = []
+
+for x, row_ in enumerate(graph):
+    for y, target in enumerate(row_):
+        if target == 'B':
+            all_W_position.append((x, y))
+
+
+answer = 0
+for next_network in all_W_position:
+    if len(all_W_position) == 0:
+        break
+    if next_network not in all_W_position:
+        continue
+    stack = [next_network]
+    visited = []
+    while len(stack) != 0:
+        curr_x, curr_y = stack.pop()
+        if (curr_x, curr_y) in visited:
+            continue
+        visited.append((curr_x, curr_y))
+        if curr_x + 1 <= row and graph[curr_x + 1][curr_y] == "B" and (curr_x + 1, curr_y) not in visited:
+            stack.append((curr_x+1, curr_y))
+        if curr_x - 1 >= 0 and graph[curr_x - 1][curr_y] == "B" and (curr_x - 1, curr_y) not in visited:
+            stack.append((curr_x-1, curr_y))
+        if curr_y + 1 <= col and graph[curr_x][curr_y + 1] == 'B' and (curr_x, curr_y + 1) not in visited:
+            stack.append((curr_x, curr_y+1))
+        if curr_y - 1 >= 0 and graph[curr_x][curr_y -1] == "B" and (curr_x, curr_y - 1) not in visited:
+            stack.append((curr_x, curr_y-1))
+    answer += len(visited)**2
+    all_W_position = list(set(all_W_position) - set(visited))
+
+print(answer)
