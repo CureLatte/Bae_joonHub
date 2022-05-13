662690311f2b458f09b4fa22fc30d982d63de468
zhqmfkv <83353283+CureLatte@users.noreply.github.com>
2022-05-13 20:33:37 +0900

    [level 2] Title: 전화번호 목록, Time: 344.40 ms, Memory: 96.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        
+        int current_len = phone_book[0].length();
+        String target = phone_book[0];
+        String prevalue = "";
+        for(int i=0; i<phone_book.length; i++){
+            // 중복확인
+            if (prevalue.compareTo(phone_book[i])==0){
+                return false;
+            } else {
+                prevalue = phone_book[i];
+            }
+            
+            // 길이가 바뀌였을 때 
+            
+            if(current_len >= phone_book[i].length() || target.compareTo(phone_book[i].substring(0, current_len)) !=0){
+                
+                target = phone_book[i];
+                current_len = phone_book[i].length();
+            }
+            else{
+                String check = phone_book[i].substring(0, current_len);
+                if(target.compareTo(check) == 0 ){
+                    return false;
+                }
+                
+            }
+            
+        }
+        
+        return true;
+    }
+}
