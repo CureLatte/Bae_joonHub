c78d78c6ae586764a28842dfe03e52dfe9bcac06
zhqmfkv <83353283+CureLatte@users.noreply.github.com>
2022-05-28 11:42:25 +0900

    [Bronze III] Title: A+B - 8, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,7 @@
+import sys
+test_cnt = input()
+test_cnt = int(test_cnt)
+
+for k in range(1,test_cnt+1):
+    a, b = map(int, sys.stdin.readline().split())
+    print(f'Case #{k}: {a} + {b} = {a + b}')
