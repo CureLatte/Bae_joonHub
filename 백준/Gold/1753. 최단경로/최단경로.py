a0e0f21aa65d62b3b25528b17e1af264ce0ff01c
zhqmfkv <83353283+CureLatte@users.noreply.github.com>
2022-06-04 00:10:41 +0900

    [Gold V] Title: 최단경로, Time: 804 ms, Memory: 73388 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,29 +1,43 @@
+# 다익스트라 연습장
+# Minimum Spanning Tree
+
 import sys
+from collections import deque
 import heapq
-input = sys.stdin.readline
-INF = sys.maxsize
 
-V, E = map(int, input().split())
-K = int(input())
-edge = [ [] for _ in range(V+1)]
-dist = [INF] * (V+1)
 
-for i in range(E):
-    u,v,w = map(int, input().split())
-    edge[u].append([w,v])
+def main():
+    input = sys.stdin.readline
+    INF = sys.maxsize
 
-# 시작점 초기화
-dist[K] = 0
-heap = [[0,K]]
+    n, e = map(int, input().split())
+    k = int(input())
 
-while heap:
-    ew, ev = heapq.heappop(heap)
-    if dist[ev] != ew: continue
-    for nw, nv in edge[ev]:
-        if dist[nv] > ew + nw:
-            dist[nv] = ew + nw
-            heapq.heappush(heap, [dist[nv], nv])
+    edge = [[] for _ in range(n + 1)]
+    dist = [INF] * (n + 1)
 
-for i in range(1, V+1):
-    if dist[i] == INF: print("INF")
-    else: print(dist[i])
+    for _ in range(e):
+        u, v, w = map(int, input().split())
+        edge[u].append([w, v])
+
+    heap = [[0, k]]
+    dist[k] = 0
+
+    while heap:
+        ew, ev = heapq.heappop(heap)
+        if dist[ev] != ew:
+            continue
+        for nw, nv in edge[ev]:
+            if dist[nv] > ew + nw:
+                dist[nv] = ew + nw
+                heapq.heappush(heap, [dist[nv], nv])
+
+    for i in range(1, n + 1):
+        if dist[i] == INF:
+            print("INF")
+        else:
+            print(dist[i])
+
+
+if __name__ == '__main__':
+    main()
